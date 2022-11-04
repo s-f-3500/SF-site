79f074cb3d00e8e74b0c4890efcbf7bda57a57d5
KyleTheUndead <kyletheundead.tk@gmail.com>
2022-11-04 13:17:44 +0100

    new

---
+++ js/minecraft.js
@@ -0,0 +1,23 @@
+var url = "https://api.minetools.eu/ping/mhfarm.minehut.gg/25565";
+ 
+$.getJSON(url, function(result) {
+    //data is the JSON string
+if(result.players.max == 0){
+    $('#mc-status').html('Offline');
+    $('#mc-version').html(result.version.name.replace('Velocity ', '').replace('-', ' - '));
+    $('#mc-players').html("/");
+}else{
+    $('#mc-status').html('Online');
+    $('#mc-version').html(result.version.name.replace('Velocity ', '').replace('-', ' - '));
+    $('#mc-players').html(result.players.online + "/" + result.players.max);
+}
+});
+
+document.querySelector(".tooltip").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "block";
+});
+
+
+document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+});
